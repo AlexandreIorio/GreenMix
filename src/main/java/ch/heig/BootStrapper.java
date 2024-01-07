a989a7ba05892a2d639e926c074deaee1ccc4199
CoJaques <colin.jaques@heig-vd.ch>
2024-01-07 22:41:48 +0100

    Add project config + dependency for Pico-cli and Javalin

--- src/main/java/ch/heig/BootStrapper.java
+++ src/main/java/ch/heig/BootStrapper.java
@@ -1,7 +1,13 @@
 package ch.heig;
 
+import io.javalin.Javalin;
+
 public class BootStrapper {
+    public static final int PORT = 8080;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = Javalin.create();
+        app.get("/", ctx -> ctx.result("Hello, world!"));
+        app.start(PORT);
     }
 }
