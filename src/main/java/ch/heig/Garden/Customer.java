2efb623e556bf20e8b7d14a7483e26a92988b0c9
SlWa99 <walid.slimani@hes-so.ch>
2024-01-09 14:49:47 +0100

    Ajout des classes de bases

---
+++ src/main/java/ch/heig/Garden/Customer.java
@@ -0,0 +1,14 @@
+package ch.heig.Garden;
+
+public class Customer {
+    // region Private Parameters
+    double wallet;
+    Plant[] plants;
+    // endregion
+
+    // region Ctor
+
+    // endregion
+
+
+}
---
+++ src/main/java/ch/heig/Garden/Plant.java
@@ -0,0 +1,27 @@
+package ch.heig.Garden;
+
+public class Plant {
+    // region Private Parameters
+    private final int harvest;
+    private final int money;
+    private final double duration;
+    // endregion
+
+    // region Type of plants
+    enum plantsType {
+
+    }
+    // endregion
+
+    // region Ctor
+    public Plant(int harvest, int money, double duration) {
+        this.harvest = harvest;
+        this.money = money;
+        this. duration = duration;
+    }
+    // endregion
+
+    // region Public Method
+    public
+    // endregion
+}
